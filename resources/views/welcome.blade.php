d85ec480a9c990c17c5dd3139a9e2730fae68ac9
Exar <you@example.com>
2020-10-10 17:04:17 -0500

    nombre pagina web

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<nav class = "navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="#"> <font style = "vertical-align: heredar;"> <font style = "vertical-align: heredar;"> Programacion Web con Frameworks </font> </ fuente> </a>
+  <button class = "navbar-toggler" type = "button" data-toggle = "collapse" data-target = "# navbarColor01" aria-controls = "navbarColor01" aria-extended = "false" aria-label = "Navegación de palanca ">
+    <span class = "navbar-toggler-icon"> </span>
+  </button>
+  </nav>
+  <br>
+  <div class = "card text-white bg-primary mb-3" style = "max-width: 30rem;">
+  <div h2 class = "card-header"> <font style = "vertical-align: heredar;"> <font style = "vertical-align: heredar;"> Alumno </font> </font> </div>
+  <div class = "card-body">
+    <h4 class = "card-title"> <font style = "vertical-align: heredar;"> <font style = "vertical-align: heritage;"> Exar Eleaquim Garcia Lopez </font> </font> 
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+  <!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
